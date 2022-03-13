13a9dd234e34fdeb88bfd97992acc3a9607421e3
Ole Nascimento <olenascimento@gmail.com>
2022-03-13 12:55:04 +0100

    added detail view

--- location/urls.py
+++ location/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path 
 from .views import LocationListView 
+from .views import LocationDetailView
 
 urlpatterns = [
-  path('', LocationListView.as_view())
+  path('', LocationListView.as_view()),
+  path('<int:pk>/', LocationDetailView.as_view()),
 ]
--- location/views.py
+++ location/views.py
@@ -1,3 +1,8 @@
+from http.client import NOT_FOUND
+from re import L
+from urllib import response
+from xml.dom import NotFoundErr
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status 
@@ -11,5 +16,23 @@ class LocationListView(APIView):
         location = Location.objects.all()
         serialized_location = LocationSerializer(location, many=True)
         return Response(serialized_location.data, status=status.HTTP_200_OK)
-        
-
+    
+    def post(self, request, format=None):
+        location_to_add = LocationSerializer(data=request.data)
+        if location_to_add.is_valid():
+            location_to_add.save()
+            return Response(location_to_add.data, status=status.HTTP_201_CREATED)
+        return Response(location_to_add.errors, status=status.HTTP_400_BAD_REQUEST)
+      
+class LocationDetailView(APIView):
+    
+    def get_object(self, pk):
+      try:
+        return Location.objects.get(pk=pk)
+      except Location.DoesNotExist:
+        raise Http404
+      
+    def get(self, _request, pk, format=None):
+      location_one = self.get_object(pk)
+      serializer = LocationSerializer(location_one)
+      return Response(serializer.data)
